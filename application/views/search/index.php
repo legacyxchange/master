32148d60406bb89e30ddec237c0f37a3034a6cb3
nickr <nickr@legacyxchange.com>
2014-12-08 18:22:40 -0600

    test

--- application/views/search/index.php
+++ application/views/search/index.php
@@ -1,4 +1,4 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>Another test
     <!--war_outer start-->
     
     <!-- jsissor Start -->	
