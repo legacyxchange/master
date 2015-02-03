d7f029cdfc8425b0ee535726a51e972ac2cf5b24
nickr <nickr@legacyxchange.com>
2015-02-03 14:11:40 -0600

    merge

--- application/views/listings/listings_menu.php
+++ application/views/listings/listings_menu.php
@@ -132,6 +132,66 @@
 							<li>
 							    <li class="dropdown">
 								<a class="dropdown-toggle" data-toggle="dropdown" href="#">
+									Stores<span class="caret"></span>
+								</a>
+								<ul class="dropdown-menu">
+									<li><a href="#" id="origional_item">Store Items?</a>
+										<div class="origional_item_container_main">
+											<div class="origional_item_container" style="display: none;">
+												<div class="origional_item_close">
+													<i class="fa fa-times-circle"></i>
+												</div>
+												<p>Stores Items are items which have been....
+												</p>
+												<p>LegacyXchange documents and follows strict
+													chain-of-custody procedures for all Original items.the
+													documentation, as well as the DNA mark, is 100% verifiable.
+												</p>
+												<p>
+													<b><i>All Stores items are .....</i></b>
+												</p>
+											</div>
+										</div></li>
+									<li class="divider"></li>
+									<li><b>Show Me</b></li>
+									<li class="divider"></li>
+									<li><a href="#">All Stores</a></li>
+									<li><a href="#">Sports Only</a>
+										<ul class="submenu">
+											<li><a href="#">Baseball</a></li>
+											<li><a href="#">Football</a></li>
+											<li><a href="#">Basketball</a></li>
+											<li><a href="#">Hockey</a></li>
+											<li><a href="#">Soccer</a></li>
+											<li><a href="#">Golf</a></li>
+											<li><a href="#">Auto Sports</a></li>
+											<li><a href="#">Fighting Sports</a></li>
+											<li><a href="#">Outdoor Sports</a></li>
+											<li><a href="#">Other</a></li>
+										</ul>
+									</li>
+									<li><a href="#">Jewelry</a>
+										<ul class="submenu">
+											<li><a href="#">Watches</a></li>
+											<li><a href="#">Rings</a></li>
+											<li><a href="#">Necklaces</a></li>
+											<li><a href="#">Earrings</a></li>
+										</ul>
+									</li>
+									<li><a href="#">Clothing</a>
+										<ul class="submenu">
+											<li><a href="#">Hats</a></li>
+											<li><a href="#">Coats</a></li>
+											<li><a href="#">Ties</a></li>
+											<li><a href="#">Handbags</a></li>
+											<li><a href="#">Other</a></li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+							<li>
+							    <li class="dropdown">
+								<a class="dropdown-toggle" data-toggle="dropdown" href="#">
 									Flash Sales<span class="caret"></span>
 								</a>
 								<ul class="dropdown-menu">
@@ -193,7 +253,7 @@
 					</span>
 				
 				
-					<div class="filter-item" style="width:400px;float:right;margin-top:-30px;">
+					<div class="filter-item" style="width:400px;float:right;margin-top:-30px;margin-right:-33px;">
 						<form>
 							<span> <input type="text" class="form-control"
 								placeholder="SELLER">
